dacf664cca5e31942faa1cf0cbc3f12af2eb01df
Vitthal Korvan <korvanvitthal@gmail.com>
2024-10-18 01:22:46 +0530

    React GSAP X timeline Updated

--- GSAP X React js/gsap x react/src/App.jsx
+++ GSAP X React js/gsap x react/src/App.jsx
@@ -3,27 +3,31 @@ import gsap from "gsap";
 import { useRef } from "react";
 
 const App = () => {
-  // const boxRef = useRef()
-
-  // useGSAP(()=>{
-  //   gsap.to(boxRef.current,{
-  //     x:600,
-  //   })
-  // })
-
-  useGSAP(()=>{
-    gsap.to(".box",{
+  const boxRef = useRef();
+  const cirRef = useRef();
+  //Setting GSAP TimeLine
+  const tl = gsap.timeline();
+  useGSAP(() => {
+    tl.to(boxRef.current, {
       x:1000,
-      delay:1,
-      duration:2,
       rotate:360,
-      backgroundColor:"blue"
+      duration:2,
+      delay:1,
+      borderRadius:"50%"
     })
-  })
-  return <div>
-    {/* <div ref={boxRef} className="box"></div> */}
-    <div className="box"></div>
-  </div>;
+    tl.to(cirRef.current, {
+      x: 1000,
+      rotate: 360,
+      duration: 2,
+      borderRadius: "0%",
+    });
+  });
+  return (
+    <>
+      <div ref={boxRef} className="box"></div>
+      <div ref={cirRef} className="circle"></div>
+    </>
+  );
 };
 
 export default App;
